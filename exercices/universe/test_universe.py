128349ea617f11aa47c8ecda3209f78ad352ca90
awalter515 <90966590+awalter515@users.noreply.github.com>
2021-09-18 16:08:12 +0200

    Update test_universe.py

--- exercices/universe/test_universe.py
+++ exercices/universe/test_universe.py
@@ -1,6 +1,3 @@
-import sys
+y = mafonction(1)
 
-y = sys.stdout
-
-print(y)
-assert y ==  "La réponse est 42" , "Ceci n'est pas la bonne réponse"
+assert y ==  42 , "Ceci n'est pas la bonne réponse"
