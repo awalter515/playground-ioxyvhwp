cd612fbfa4316f53460ecf07cccf02db0adfadb3
awalter515 <90966590+awalter515@users.noreply.github.com>
2021-09-18 16:25:14 +0200

    Update universe.py

--- exercices/universe/universe.py
+++ exercices/universe/universe.py
@@ -2,7 +2,7 @@ def mafonction(n) :
     #Vous devez corriger ce code pour résoudre l'exerice, ne touchez pas à ce qui précède
     
     x = 41
-    print(f"la réponse est {x}")
+    print(f"The answer is {x}")
     
     #Ne touchez pas à la ligne suivante !
     return x
