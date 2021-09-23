637e75de2b70031d0c55ea769cdc6e3d74c7769d
awalter515 <90966590+awalter515@users.noreply.github.com>
2021-09-23 22:26:01 +0200

    Update test_universe.py

--- exercices/universe/test_universe.py
+++ exercices/universe/test_universe.py
@@ -6,15 +6,9 @@ from universe import mafonction
 y = mafonction(1)
 reponse = 42
 
-a = sys.stdout.readlines()
 
-print(a)
-'''
 if y != reponse :
   print(f"Votre réponse est fausse, pensez au guide du voyageur galactique")
-  raise AssertionError("Faute dans le résultat")
+  raise NameError("Faute dans le résultat")
   
 
-
-assert y ==  reponse , "Ceci n'est pas la bonne réponse"
-'''
