935f2509d4ab1479e23cbdd52136b74ada52b0ba
awalter515 <90966590+awalter515@users.noreply.github.com>
2021-09-23 22:29:29 +0200

    Update test_universe.py

--- exercices/universe/test_universe.py
+++ exercices/universe/test_universe.py
@@ -7,6 +7,9 @@ reponse = 42
 
 if y != reponse :
   print(f"ERREUR Votre réponse est fausse, pensez au guide du voyageur galactique")
-  raise AssertionError("Faute dans le résultat")
+  raise AssertionError
+  
+else :
+  print(\n "BRAVO, C'EST LA BONNE REPONSE")
   
 
